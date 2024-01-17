e6d34c276560b818c39a9307af53fb34cdad035b
AdNyk <adithya_nayak@hotmail.com>
2024-01-17 19:15:27 +0530

    testing recording

---
+++ Solving problems on array/LongestSubarrayWithSumK/longest subarray with sum k 2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    map<int, long long> cumulativeSum={};
+    cumulativeSum.first()=0
+    for(auto i=0;i<a.size();i++){
+    }
+}
+int main(){
+
+}
